0954e989d0cfac1848eeaa518d8bef7ca2841ee8
Dalibor Koenig <dalibor.koenig@gmail.com>
2021-08-12 18:33:23 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import logo from './Images/logo.svg';
 
 function App() {
+  const fizzBuzzArr = [];
+  const fizzBuzz = () => {
+    let value = 1;
+    while (value < 100) {
+      if(value % 3 === 0 && value % 5 === 0) {
+        fizzBuzzArr.push('FizzBuzz');
+      }
+      else if(value % 3 === 0) {
+        fizzBuzzArr.push('Fizz');
+      }
+      else if(value % 5 === 0) {
+        fizzBuzzArr.push('Buzz');
+      }
+      else {
+        fizzBuzzArr.push(value);
+      }
+      value++;
+    }
+    return fizzBuzzArr
+  }
+  const fizzBuzzLog = () => {
+    for (var i=1; i < 101; i++){
+      if (i % 15 == 0) console.log("FizzBuzz");
+      else if (i % 3 == 0) console.log("Fizz");
+      else if (i % 5 == 0) console.log("Buzz");
+      else console.log(i);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} />
+      <button onClick={()=>fizzBuzzLog()}>Click me for console log</button>
+      {fizzBuzz().map(i=> i==="Fizz" ? <p key={Math.random()}>I am divisable with 3 so... {i}</p> : i==="Buzz" ? <p key={Math.random()}>I am divisable with 5 so... {i}</p> : i==="FizzBuzz" ? <p key={Math.random()}>I am divisable with 3 and 5 so... {i}</p> : <p key={Math.random()}>I am a nerd so... {i}</p>)}
+
     </div>
-  );
+  );  
 }
 
 export default App;
