7b931adde267f4a4fee536de8da509250aa28d8a
Dalibor Koenig <dalibor.koenig@gmail.com>
2021-08-12 18:42:33 +0200

    done

--- src/App.js
+++ src/App.js
@@ -23,12 +23,7 @@ function App() {
     return fizzBuzzArr
   }
   const fizzBuzzLog = () => {
-    for (var i=1; i < 101; i++){
-      if (i % 15 == 0) console.log("FizzBuzz");
-      else if (i % 3 == 0) console.log("Fizz");
-      else if (i % 5 == 0) console.log("Buzz");
-      else console.log(i);
-    }
+    for (let i=1; i <= 100; i++)i % 15 == 0 ? console.log("FizzBuzz") :i % 3 == 0 ? console.log("Fizz") : i % 5 == 0 ? console.log("Buzz") : console.log(i);
   }
   return (
     <div className="App">
